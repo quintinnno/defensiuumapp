82f46f29c4d01c63ecbaf073913f9a32d8c2802e
José Quintinno <josequintino@hotmail.com.br>
2025-02-13 16:24:22 -0300

    🔵 feature: Implementar tabmenu

--- src/app/components/tabmenu/tabmenu.page.ts
+++ src/app/components/tabmenu/tabmenu.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { CredencialPage } from 'src/app/pages/credencial/credencial.page';
 
 @Component({
   selector: 'app-tabmenu',
@@ -8,8 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabmenuPage implements OnInit {
 
+  private modalController = inject(ModalController);
+
   constructor() { }
 
   ngOnInit() {}
+
+  public async apresentarModalCredencialCreate() {
+    const modal = await this.modalController.create({
+      component: CredencialPage,
+      initialBreakpoint: 0.5,
+      breakpoints: [0, 0.25, 0.5, 0.85],
+      cssClass: "modal-custom"
+    });
+    modal.present();
+  }
 
 }
